654ba50b2a7206c9f5bd91cbdee01317dd432662
Aundre Labrador <aundrejlabrador@student.hartnell.edu>
2020-09-25 17:58:51 -0700

    working flask application template

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# How to Run Server in Terminal
+#
+# export FLASK_APP="app.py"
+# export FLASK_DEBUG=1
+# flask run
+#
+
+@app.route('/')
+def index():
+    return "Hello World"
