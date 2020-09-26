5d79ad34808cf4c033ca6a38f31ffa2d96d8f895
ericchavez831 <ericchavez831@gmail.com>
2020-09-26 15:14:27 -0700

    Pushing app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 from wifi import Wifi
 from food_drive import FoodDrive
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 # How to Run Server in Terminal
@@ -10,15 +11,37 @@ app = Flask(__name__)
 # export FLASK_DEBUG=1
 # flask run
 #
-wifi = [
-    ['McDonalds', 36.676770, -121.656470],
-    ['Starbucks', 36.716793, -121.657716],
-    ['Walmart', 36.713826, -121.653225]
-]
 
-print(FoodDrive().find_food_drive())
-print(Wifi().find_wifi())
+food_drive_dic = (FoodDrive().find_food_drive())
+wifi_dic = (Wifi().find_wifi())
+geolocator = Nominatim(user_agent="myGeocoder")
+
+food_drive_coordinates = []
+del food_drive_dic['Henry F. Kammann Elementary']
+for key, value in food_drive_dic.items():
+	loc = geolocator.geocode(value)
+	lat = loc.latitude
+	lng = loc.longitude
+
+	food_drive_coordinates.append((key,value,lat,lng))
+
+
+# Need to get wifi coodinates to work
+# **********************************
+
+# What we couldn't get to work
+
+# wifi_coordinates = []
+# for key, value in wifi_dic.items():
+# 	loc = geolocator.geocode1(value)
+# 	if(loc):
+# 		lat = loc.latitude
+# 		lng = loc.longitude
+# 		wifi_coordinates.append((value,lat,lng))
+
+# print(wifi_coordinates)
+
 
 @app.route('/')
 def index():
-    return render_template("map.html", places=wifi)
+    return render_template("map.html", places=food_drive_coordinates)
