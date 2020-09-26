34ca9207ea29be85fe525f9adcf7cdd1a6a21507
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2020-09-25 18:12:31 -0700

    added name to file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Developers: Aundre Labrador, Edward Cluster, Prince Rios, Eric Chavez
+# Developers: Aundre Labrador, Edward Cluster, Prince Rios, Eric Chavez, Alex Espinoza-Fuentes
 from flask import Flask, render_template
 
 app = Flask(__name__)
